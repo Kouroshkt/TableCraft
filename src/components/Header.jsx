7bb1dba2e193280a4f1cc045944848cd1682eadb
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2025-07-05 18:11:32 +0200

    Create Header.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,47 @@
 import { useState } from "react"
+import "../styles/Header.css"
 
 export function Header() {
     const [showForm, setShowForm] = useState(false);
-    const logning=()=>{
-        setShowForm(true)
+    const [showCraeteAccount, setShowCreateAccount] = useState(false);
+    const logning = () => {
+        setShowForm(true);
+        setShowCreateAccount(false);
+    }
+    const CreateAccount = () => {
+        setShowCreateAccount(true);
+        setShowForm(false);
     }
 
     return (
-        <div>
-            <button onClick={logning}>Log in</button>
-            {showForm &&
-                (<div>
-                    <label>User</label>
-                    <input type="text" />
-                    <label>Password</label>
-                    <input type="text" />
-                </div>)
-            }
+        <div className="headerSection">
+            <div className="buttons">
+                <button onClick={logning}>Log in</button>
+                {showForm &&
+                    (<div className="login">
+                        <label>User</label>
+                        <input type="text" />
+                        <label>Password</label>
+                        <input type="text" />
+                        <button>Login</button>
+                        <button onClick={() => setShowForm(false)}>X</button>
+                    </div>
+                    )}
+                {showCraeteAccount &&
+                    (<div className="login">
+                        <label>User</label>
+                        <input type="text" />
+                        <label>Password</label>
+                        <input type="text" />
+                        <label>Reapet Password</label>
+                        <input type="text" />
+                        <button>Create account</button>
+                        <button onClick={() => setShowCreateAccount(false)}>X</button>
+                    </div>
+                    )}
+                <button onClick={CreateAccount}>Create account</button>
+            </div>
+
         </div>
     )
 }
